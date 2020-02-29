074b5cf1c0d9d59084bc385c1b9d9921ab14a99e
Songren Zhao <songrenzhao@gmail.com>
2020-02-29 15:39:00 -0500

    added ports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import createApp from './app';
-import config from './config';
 
 const app = createApp();
 
-const port = config.env.PORT || 8080;
-
 // eslint-disable-next-line no-console
-app.listen(config.env.PORT || 8080, () => console.log(`Server on Port ${port}`));
+app.listen(process.env.PORT || 8080, () => console.log(`Server on Port ${port}`));
